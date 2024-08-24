944c5be8ca488c627c6a41c444e313e1cd9633a4
Harsh <harsh20221@gmail.com>
2024-08-24 20:20:42 +0530

    More changes and Making a Text Label for Displaying user Messages

--- Giga_Chat/ChatViewController.swift
+++ Giga_Chat/ChatViewController.swift
@@ -9,6 +9,12 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages :[Message] = [ ////? These are Dummy Messages that we'll display to check
+    Message(sender: "123@gmai.com", body: "Hello This is a Test"),
+    Message(sender: "456@gmail.com", body: "If you're seeing this then it's working "),
+    Message(sender: "786", body: "I believe it is Working"),
+    Message(sender: "91123", body: "Some final Checks and we'll be done ")
+    ]
     @IBAction func Logoutbutton(_ sender: UIBarButtonItem) {
         let firebaseAuth = Auth.auth()
         do {
@@ -29,4 +35,27 @@ class ChatViewController: UIViewController {
     }
     
 
+}
+extension ChatViewController :UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //This is a part of necessary protocols responsible for creating the Controller
+        return messages.count ///This will return the no of rows that we need to store our messages
+    }
+    
+  
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { ///This is also a necessary protocol for the chat controller
+        let cell = tableView.dequeueReusableCell(withIdentifier:"ReusableCell" , for: indexPath)    ///Here you can return the        type of cell that you will need in your table view
+        if #available(iOS 14.0, *) { ///Since textlabel is now depracted so we make an if else statement that will use the now latest .defaultcontent Configrator  if We have a supported IOS Device else it'll use the legacy textlabel instead 
+            var content = cell.defaultContentConfiguration()
+            content.text = messages[indexPath.row].body
+            cell.contentConfiguration = content
+            return cell
+        } else {
+            // Fallback on earlier versions
+            cell.textLabel?.text = messages[indexPath.row].body
+            return cell
+        }
+        
+    }
+    
+    
 }
---
+++ Giga_Chat/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Giga_Chat
+//
+//  Created by Harsh Kumar on 24/08/24.
+//
+////This will work as a Model For Creating Messages from Now on
+import Foundation
+struct Message {
+    let sender : String
+    let body:String
+}
