2a4548e469d2da485910bd4ec73c6f75ac031c7e
Harsh <harsh20221@gmail.com>
2024-08-26 06:20:50 +0530

    Dismissed Navigation Bar on Starting the Application

--- Giga_Chat/ViewController.swift
+++ Giga_Chat/ViewController.swift
@@ -8,12 +8,19 @@ import CLTypingLabel
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel!
-    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated) //Make sure to add this super method to ensure it executes safely 
+        navigationController?.isNavigationBarHidden=true //Hide Navigation bar when loading into the App
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.isNavigationBarHidden=false //Make Navigation Bar Reappear after finally lOADING Up
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-  
+        
         titleLabel.text="🦖GigaChat";
-       
+      
     }
     
 
